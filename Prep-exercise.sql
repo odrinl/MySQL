b904c532f46f25bd9ede712c76ceb976c82a4633
Audrey <83685230+odrinl@users.noreply.github.com>
2023-08-23 17:11:41 +0100

    implemented mentor's comments

--- Prep-exercise.sql
+++ Prep-exercise.sql
@@ -1,59 +1,46 @@
 CREATE TABLE `categories` (
-  `CategoryID` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `CategoryName` varchar(255) NOT NULL,
-  PRIMARY KEY (`CategoryID`)
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `ingredients` (
-  `IngredientID` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `IngredientName` varchar(255) NOT NULL,
-  PRIMARY KEY (`IngredientID`)
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `quantities` (
-  `QuantityID` int NOT NULL AUTO_INCREMENT,
-  `QuantityValue` float DEFAULT NULL,
-  PRIMARY KEY (`QuantityID`)
-);
-
-CREATE TABLE `recipecategories` (
+CREATE TABLE `recipe_categories` (
   `RecipeID` int NOT NULL,
   `CategoryID` int NOT NULL,
   PRIMARY KEY (`RecipeID`,`CategoryID`),
-  KEY `CategoryID` (`CategoryID`),
-  CONSTRAINT `recipecategories_ibfk_1` FOREIGN KEY (`RecipeID`) REFERENCES `recipes` (`RecipeID`),
-  CONSTRAINT `recipecategories_ibfk_2` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`)
+  CONSTRAINT `recipe_categories_ibfk_1` FOREIGN KEY (`RecipeID`) REFERENCES `recipes` (`id`),
+  CONSTRAINT `recipe_categories_ibfk_2` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`id`)
 );
 
-CREATE TABLE `recipeingredients` (
+CREATE TABLE `recipe_ingredients` (
   `RecipeID` int NOT NULL,
   `IngredientID` int NOT NULL,
-  `QuantityID` int DEFAULT NULL,
-  `UnitID` int DEFAULT NULL,
+  `QuantityValue` float DEFAULT NULL,
+  `UnitName` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`RecipeID`,`IngredientID`),
-  KEY `IngredientID` (`IngredientID`),
-  KEY `recipeingredients_FK` (`QuantityID`),
-  KEY `recipeingredients_FK_1` (`UnitID`),
-  CONSTRAINT `recipeingredients_FK` FOREIGN KEY (`QuantityID`) REFERENCES `quantities` (`QuantityID`),
-  CONSTRAINT `recipeingredients_FK_1` FOREIGN KEY (`UnitID`) REFERENCES `units` (`UnitID`),
-  CONSTRAINT `recipeingredients_ibfk_1` FOREIGN KEY (`RecipeID`) REFERENCES `recipes` (`RecipeID`),
-  CONSTRAINT `recipeingredients_ibfk_2` FOREIGN KEY (`IngredientID`) REFERENCES `ingredients` (`IngredientID`)
+  CONSTRAINT `recipe_ingredients_ibfk_1` FOREIGN KEY (`RecipeID`) REFERENCES `recipes` (`id`),
+  CONSTRAINT `recipe_ingredients_ibfk_2` FOREIGN KEY (`IngredientID`) REFERENCES `ingredients` (`id`)
 );
 
-CREATE TABLE `recipes` (
-  `RecipeID` int NOT NULL AUTO_INCREMENT,
-  `RecipeName` varchar(255) NOT NULL,
-  PRIMARY KEY (`RecipeID`)
-);
-
-CREATE TABLE `recipesteps` (
+CREATE TABLE `recipe_steps` (
   `RecipeID` int NOT NULL,
   `StepID` int NOT NULL,
   `StepOrder` int DEFAULT NULL,
   PRIMARY KEY (`RecipeID`,`StepID`),
-  KEY `StepID` (`StepID`),
-  CONSTRAINT `recipesteps_ibfk_1` FOREIGN KEY (`RecipeID`) REFERENCES `recipes` (`RecipeID`),
-  CONSTRAINT `recipesteps_ibfk_2` FOREIGN KEY (`StepID`) REFERENCES `steps` (`StepID`)
+  CONSTRAINT `recipe_steps_ibfk_1` FOREIGN KEY (`RecipeID`) REFERENCES `recipes` (`id`),
+  CONSTRAINT `recipe_steps_ibfk_2` FOREIGN KEY (`StepID`) REFERENCES `steps` (`StepID`)
+);
+
+CREATE TABLE `recipes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `RecipeName` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `steps` (
@@ -61,9 +48,3 @@ CREATE TABLE `steps` (
   `StepDescription` text NOT NULL,
   PRIMARY KEY (`StepID`)
 );
-
-CREATE TABLE `units` (
-  `UnitID` int NOT NULL AUTO_INCREMENT,
-  `UnitName` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`UnitID`)
-);   
